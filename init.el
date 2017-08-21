16370463dc699a97e9eb71e62b9867af0920549c
Simon Zelazny <zelazny@mailbox.org>
2017-08-21 17:20:00 +0200

    clean up org-present start/stop hooks

--- init.el
+++ init.el
@@ -15,6 +15,7 @@
 ;; My custom globals
 (defvar global-font-height 132)
 (defvar global-font-face "Iosevka Term") ;Iosevka Term")
+(defvar original-mode-line-format mode-line-format)
 
 (global-set-key (kbd "<f4>") 'evil-mode)
 ;; Prevent evil mode from opening undo-tree
@@ -168,14 +169,37 @@
 
 ;; ORG-PRESENT
 (autoload 'org-present "org-present" nil t)
+
+(defun org-present-pzel-start-hook ()
+  (interactive)
+  (setq global-font-height 362)
+  (setq cursor-type 'bar)
+  (set-cursor-color "gray")
+  (fringe-mode '(0 . 0))
+  (set-frame-parameter nil 'internal-border-width 40)
+  (set-window-margins (get-buffer-window) 3)
+  (set-face-attribute 'default nil :height global-font-height)
+  (hide-global-modeline)
+  (org-display-inline-images))
+
+(defun org-present-pzel-stop-hook ()
+  (interactive)
+  (setq global-font-height 132)
+  (setq cursor-type 'box)
+  (set-cursor-color "black")
+  (set-frame-parameter nil 'internal-border-width 0)
+  (set-window-margins (get-buffer-window) 0)
+  (set-face-attribute 'default nil :height global-font-height)
+  (show-global-modeline)
+  (org-remove-inline-images))
+
 (eval-after-load "org-present"
   '(progn
-     (add-hook 'org-present-mode-hook
-               (lambda ()
-                 (org-display-inline-images)))
-     (add-hook 'org-present-mode-quit-hook
-               (lambda ()
-                 (org-remove-inline-images)))))
+     (add-hook 'org-present-mode-hook 
+               (lambda () (org-present-pzel-start-hook)))
+     (add-hook 'org-present-mode-quit-hook 
+               (lambda () (org-present-pzel-stop-hook)))))
+
 ;; ISPELL
 (setq ispell-program-name "/usr/bin/hunspell")
 
@@ -346,6 +370,14 @@
     (normal-mode)
     (funcall v)
     (mapcar 'funcall mm)))
+
+(defun hide-global-modeline ()
+  (interactive)
+  (setq mode-line-format nil))
+
+(defun show-global-modeline ()
+  (interactive)
+  (setq mode-line-format original-mode-line-format))
 
 (defun global-font-size-bigger ()
   (interactive)
